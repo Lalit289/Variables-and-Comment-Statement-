704eea37d63010b2db7e270b40247472708629f1
Lalit289 <87890683+Lalit289@users.noreply.github.com>
2021-12-27 13:30:19 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,15 @@
+lalit = 89 - 59
+
+#print is to display things on terminal
+# LAlit is bad boy
+""" 
+jgdfjgk
+gffdjgdfkjg
+dfgjdjghkdvsd
+fdhfksdjfs
+;sdlhfjdsfldarsaerd
+fcdejfgdshfkdjf
+"""
+print(lalit)
+
+
